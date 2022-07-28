2c8d04bdc061310c35d0ca2daeb5f0c5f3504187
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-07-28 17:29:20 +0900

    station7 クリア

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,9 +9,35 @@
 </head>
 
 <body>
-    @foreach ($movies as $movie)
-        {{ $movie }}
-    @endforeach
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>映画タイトル</th>
+            <th>画像URL</th>
+            <th>公開年</th>
+            <th>上映中かどうか</th>
+            <th>概要</th>
+            <th>登録日時</th>
+            <th>更新日時</th>
+        </tr>
+        @foreach ($movies as $movie)
+            <tr>
+                <td>{{ $movie->id }}</td>
+                <td>{{ $movie->title }}</td>
+                <td><img src={{ $movie->image_url }} width="100"></td>
+                <td>{{ $movie->published_year }}</td>
+                @if ($movie->is_showing == true)
+                    <td>上映中</td>
+                @else
+                    <td>上映予定</td>
+                @endif
+                <td>{{ $movie->description }}</td>
+                <td>{{ $movie->created_at }}</td>
+                <td>{{ $movie->updated_at }}</td>
+            </tr>
+        @endforeach
+    </table>
+
 </body>
 
 </html>
