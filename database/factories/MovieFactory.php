a9182ebd216e216863d2e473e5767ac6d75e4695
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-07-28 16:54:04 +0900

    moviesのカラム追加とmigration、factoryの処理

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -16,6 +16,9 @@ class MovieFactory extends Factory
         return [
             'title' => $this->faker->unique()->word,
             'image_url' => $this->faker->imageUrl(),
+            'published_year' => $this->faker->numberBetween($min = 1940, $max = 2022),
+            'is_showing' => $this->faker->numberBetween($min = 0, $max = 1),
+            'description' => $this->faker->realTextBetween($minNbChars = 30, $maxNbChars = 100, $indexSize = 5),
             'created_at' => $this->faker->dateTime($max = 'now', $timezone = null),
             'updated_at' => $this->faker->dateTime($max = 'now', $timezone = null),
         ];
