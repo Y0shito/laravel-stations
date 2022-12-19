da11aa85affeda81088186972ed65e4f9a26083c
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-12-19 21:56:16 +0900

    admin_movies_idページレイアウト修正

--- resources/views/admin_movies_id.blade.php
+++ resources/views/admin_movies_id.blade.php
@@ -5,28 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $movie->title }}</title>
+    <link rel="stylesheet" href="{{ asset('css/admin_movies_id.css') }}">
+    <title>StationMovies/管理者画面/映画個別確認/{{ $movie->title }}</title>
 </head>
 
 <body>
-    <h2>映画個別管理画面</h2>
+    <header>
+        <p><a href="{{ route('adminMovies') }}">StationMovies/管理者画面/映画個別確認</a></p>
+    </header>
+
     <h2>{{ $movie->title }}</h2>
     <img src={{ $movie->image_url }} width="400">
     <p>ID：{{ $movie->id }}</p>
-    <p>公開日：{{ $movie->published_year }}</p>
-    <p>上映状況：{{ $movie->is_showing == true ? '上映中' : '上映予定' }}</p>
+    <p>公開年：{{ $movie->published_year }}</p>
+    <p>公開状況：{{ $movie->is_showing == true ? '上映中' : '上映予定' }}</p>
     <p>概要：{{ $movie->description }}</p>
+    <p>作成日：{{ $movie->created_at }}</p>
+    <p>更新日：{{ $movie->updated_at }}</p>
 
-    @foreach ($movie->schedules as $schedule)
-        <table border="1">
-            <tr>
-                <th>ID</th>
-                <th>作品ID</th>
-                <th>開始時刻</th>
-                <th>終了時刻</th>
-                <th>作成日時</th>
-                <th>更新日時</th>
-            </tr>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>作品ID</th>
+            <th>開始時刻</th>
+            <th>終了時刻</th>
+            <th>作成日時</th>
+            <th>更新日時</th>
+        </tr>
+        @foreach ($movie->schedules as $schedule)
             <tr>
                 <td>{{ $schedule->id }}</td>
                 <td>{{ $schedule->movie_id }}</td>
@@ -35,14 +41,11 @@
                 <td>{{ $schedule->created_at }}</td>
                 <td>{{ $schedule->updated_at }}</td>
             </tr>
-        </table>
-        <a href="{{ route('scheduleManage', ['id' => $schedule->movie_id]) }}">
-            <p>「{{ $movie->title }}」のスケジュール管理へ</p>
-        </a>
-    @endforeach
-
-    <p>作成日：{{ $movie->created_at }}</p>
-    <p>更新日：{{ $movie->updated_at }}</p>
+        @endforeach
+    </table>
+    <a href="{{ route('scheduleManage', ['id' => $schedule->movie_id]) }}">
+        <p>「{{ $movie->title }}」のスケジュール管理へ</p>
+    </a>
 </body>
 
 </html>
