badd7df8bd645089e88d64bfc27ee01a6d1e9b0d
Y0shito <75707750+Y0shito@users.noreply.github.com>
2022-12-30 18:26:46 +0900

    外部キーの参照先を'sheets'から'screens'へ

---
+++ database/migrations/2022_12_30_172148_edit_reservations_index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropForeign('reservations_sheet_id_foreign');
+            $table->foreign('sheet_id')->references('id')->on('screens');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropForeign('reservations_sheet_id_foreign');
+            $table->foreign('sheet_id')->references('id')->on('sheets');
+        });
+    }
+};
